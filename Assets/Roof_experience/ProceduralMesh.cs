6ffdd0254b97c2dd56eaab6d4295558d959343ea
FunProgger <jjberry930920@gmail.com>
2024-07-08 14:40:35 +1200

    update 08/07
    Added roof measurement customisation

--- Assets/Roof_experience/ProceduralMesh.cs
+++ Assets/Roof_experience/ProceduralMesh.cs
@@ -32,11 +32,6 @@ public class ProceduralMesh : MonoBehaviour
     float y_0 = 0;
     float z_0 = 0;
 
-    //public float x_0_r = 0;
-    //public float y_0_r = 0;
-    //public float z_0_r = 0;
-    //public float w_0_r = 0;
-
     public bool changeState;
 
     public bool showTriangle_R1;
@@ -49,11 +44,6 @@ public class ProceduralMesh : MonoBehaviour
     Vector3[] Label_inside_2_h;
     Vector3[] Label_inside_2_w;
 
-    bool populated = false;
-
-
-
-
     public GameObject labelObject;
     private List<GameObject> labels = new List<GameObject>();
 
@@ -61,14 +51,15 @@ public class ProceduralMesh : MonoBehaviour
     public GameObject formula_UI;
     public GameObject homeScreen;
     public GameObject tutorialScreen;
-    
+    public GameObject Cust_UI;
 
-    public bool firstTime = true;
+    bool firstTime = true;
 
     public Sprite[] formula_illus;
 
     public UnityEngine.UI.Button m_nextButton, m_prevButton;
     public int state = 0;
+    public int cust_button_state;
 
     void Start()
     {
@@ -96,10 +87,6 @@ public class ProceduralMesh : MonoBehaviour
         vertices = new Vector3[] { vert_0, vert_1, vert_2, vert_3, vert_4, vert_5, vert_6, vert_7, vert_8, vert_9, vert_10, vert_11, vert_12, vert_13 };
 
         mesh = GetComponent<MeshFilter>().mesh;
-        //mesh.subMeshCount = 2;
-        //Debug.Log("Amount of submesh is :" + mesh.subMeshCount);
-
-        //ln = GetComponent<LineRenderer>();
         
         settingUpLabelsChildren(9);
 
@@ -107,7 +94,6 @@ public class ProceduralMesh : MonoBehaviour
         y_0 = _player.transform.position.y;
         z_0 = _player.transform.position.z+5;
 
-        //formula_UI.SetActive(false);
         showTutorialScreen();
     }
 
@@ -135,255 +121,12 @@ public class ProceduralMesh : MonoBehaviour
 
     }
 
-    //IEnumerator Switchingbetween_FullAndPartial_Mesh()
-    //{
-    //    MakeMeshData();
-    //    RoofGenerate(0);
-
-    //    yield return new WaitForSeconds(3);
-
-    //    Debug.Log("Switch to inside roof");
-    //    MakeMeshData();
-    //    RoofGenerate(1);
-        
-
-    //}
-
-    //private void CreateMesh()
-    //{
-    //    vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0) , new Vector3(1,0,1)};
-    //    triangles = new int[] { 0, 1, 2 , 2 ,1, 3} ;
-    //}
-
-    //void RoofGenerate(int state) 
-    //{
-    //    /**
-    //     * Procedurally generate roof base on the state
-    //     * 
-    //     * @param state, is the state of the roof
-    //     *      0 - full roof 
-    //     *      1 - inside roof 
-    //     */
-        
-
-    //    Vector3 vert_0 = new Vector3(x_0, y_0, z_0);
-    //    Vector3 vert_1 = new Vector3(x_0, y_0, z_0 + 1.0f / 3.0f * length);
-    //    Vector3 vert_2 = new Vector3(x_0, y_0, z_0 + 2.0f / 3.0f * length);
-    //    Vector3 vert_3 = new Vector3(x_0, y_0, z_0 + length);
-
-    //    // mid
-    //    Vector3 vert_4 = new Vector3(x_0 - (width) / 2, y_0 + height, z_0 + 1.0f / 3.0f * length);
-    //    Vector3 vert_5 = new Vector3(x_0 - (width) / 2, y_0 + height, z_0 + 2.0f / 3.0f * length);
-
-    //    // top 
-    //    Vector3 vert_6 = new Vector3(x_0 - width, y_0, z_0);
-    //    Vector3 vert_7 = new Vector3(x_0 - width, y_0, z_0 + 1.0f / 3.0f * length);
-    //    Vector3 vert_8 = new Vector3(x_0 - width, y_0, z_0 + 2.0f / 3.0f * length);
-    //    Vector3 vert_9 = new Vector3(x_0 - width, y_0, z_0 + length);
-
-    //    // inside
-    //    Vector3 vert_10 = new Vector3(x_0 - width / 2, y_0, z_0);
-    //    Vector3 vert_11 = new Vector3(x_0 - width / 2, y_0, z_0 + 1.0f / 3.0f * length);
-    //    Vector3 vert_12 = new Vector3(x_0 - width / 2, y_0, z_0 + 2.0f / 3.0f * length);
-    //    Vector3 vert_13 = new Vector3(x_0 - width / 2, y_0, z_0 + length);
-        
-
-
-    //    vertices = new Vector3[] { vert_0, vert_1, vert_2, vert_3, vert_4, vert_5, vert_6, vert_7, vert_8, vert_9, vert_10, vert_11, vert_12, vert_13 };
-
-    //    Vector3 ln_offset = new Vector3(0, 0, -0.05f);
-    //    Label_inside_1_h = new Vector3[] { vert_4 + ln_offset, vert_11 + ln_offset };
-    //    Label_inside_1_w = new Vector3[] { vert_1 + ln_offset, vert_11 + ln_offset };
-
-
-    //    //Vector3[] R1 = new Vector3[] { vert_1, vert_4 };
-
-    //    //Vector3[] T3 = new Vector3[] { vert_0, vert_4, vert_1 };
-        
-
-    //    //if (!populated)
-    //    //{
-    //    //    settingUpLabelsChildren();
-    //    //    populated = true;
-    //    //}
-
-    //    ////Amount of Submesh
-    //    //mesh.subMeshCount = 2;
-
-    //    if ( state == 0 )
-    //    {
-    //        // whole roof triangle
-    //        triangles = new int[] {
-    //        // bot
-    //        0, 4, 1,
-    //        1, 4, 2,
-    //        2, 4, 5,
-    //        5, 3, 2, 
-
-    //        // left
-    //        0, 6, 4, 
-
-    //        // top 
-    //        6, 7, 4,
-    //        4, 7, 5,
-    //        5, 7, 8,
-    //        8, 9, 5, 
-
-    //        // right
-    //        9, 3, 5,
-    //        };
-
-    //        showTriangle_R1 = false;
-    //        showTriangle_T3 = false;
-    //        showTriangle_T2 = false;
-
-    //        formula_UI.SetActive(false);
-
-    //        for ( int i = 0; i < labels.Count; i++)
-    //        {
-    //            labels[i].SetActive(false);
-    //        }
-
-    //    }
-    //    else
-    //    {
-    //        formula_UI.SetActive(true);
-    //        for ( int i = 0; i < labels.Count; i++ )
-    //        {
-    //            labels[i].SetActive(true);
-    //        }
-    //        // inside roof triangle 
-    //        //triangles = new int[]
-    //        //{
-    //        //    // Width Plane
-    //        //    1, 7, 4,
-
-    //        //    // Length Plane
-    //        //    10, 4, 11,
-    //        //    11, 4, 12,
-    //        //    12, 4, 5,
-    //        //    5, 13, 12
-    //        //};
-    //        //mesh.Clear();
-
-            
-    //        int[] tri_1 = new int[]
-    //        {
-    //            // Width Plane
-    //            1, 7, 4,
-    //        };            
-
-            
-    //        int[] tri_2 = new int[]
-    //        {
-    //            // Length Plane
-    //            10, 4, 11,
-    //            11, 4, 12,
-    //            12, 4, 5,
-    //            5, 13, 12
-    //        };
-
-    //        int[] tri_3 = new int[]
-    //        {
-    //            0,4,1
-    //        };
-
-    //        int[] tri_4 = new int[]
-    //        {
-    //            0, 10, 4,
-    //            4, 10, 6
-    //        };
-
-    //        if (showTriangle_R1)
-    //        {
-
-    //            triangles = tri_1;
-
-    //            float R1 = Mathf.Sqrt(Mathf.Pow(width / 2, 2) + Mathf.Pow(height, 2));
-
-    //            Vector3[] R1_length_verts = new Vector3[] { vertices[4], vertices[1] };
-
-    //            Vector3 label_tri1_h = new Vector3((x_0 - width) / 2 - 0.75f, (y_0 + height) / 2, z_0 + 1.0f / 3.0f * length);
-    //            Vector3 label_tri1_w = new Vector3((vertices[1].x + vertices[11].x) / 2, y_0 - 0.5f, (vertices[1].z + vertices[11].z) / 2 + 0.5f);
-    //            Vector3 label_R1 = new Vector3((vertices[1].x + vertices[4].x) / 2 + 2.25f, (vertices[4].y) / 2, vertices[4].z - 0.5f);
-
-    //            string R1_text = "R1 = " + R1.ToString();
-
-    //            makeLabel(labels[0], Label_inside_1_h, height.ToString());
-    //            makeLabel(labels[1], Label_inside_1_w, width.ToString());
-    //            makeLabel(labels[2], R1_length_verts, R1_text);
-
-    //            labels[0].transform.position = label_tri1_h;
-    //            labels[1].transform.position = label_tri1_w;
-    //            labels[2].transform.position = label_R1;
-
-
-    //            labels[0].transform.rotation = new Quaternion(0, 0, 0, 0);
-    //            labels[1].transform.rotation = new Quaternion(0, 0, 0, 0);
-    //            labels[2].transform.rotation = new Quaternion(0, 0, 0, 0);
-
-
-    //            formula_UI.transform.position = new Vector3(-5f, 8.9f, 12.7f);
-    //            formula_UI.transform.rotation = new Quaternion(0, 0, 0, 0);
-
-    //            string R1_formula = "R1<sup>2</sup> = height<sup>2</sup> + (width/2)<sup>2</sup>";
-    //            string R1_title = "Calculating R1";
-    //            showUI(R1_formula, R1_title, formula_illus[0]);
-
-    //            _player.transform.position = new Vector3(-5, 3.5f, -3);
-    //            _player.transform.rotation = new Quaternion(0, 0, 0, 0);
-    //        }
-            
-    //        if (showTriangle_T3)
-    //        { 
-    //            triangles = tri_3;
-    //            float R1 = Mathf.Sqrt(Mathf.Pow(width / 2, 2) + Mathf.Pow(height, 2));
-    //            H = showT3(R1, vertices);
-
-    //            formula_UI.transform.position = new Vector3(-6.4f, 6, 3);
-    //            formula_UI.transform.rotation = new Quaternion(0, 1, 0, -1);
-
-    //            string formula_text = "H<sup>2</sup> = R1<sup>2</sup> + Width<sup>2</sup>";
-    //            string title_text = "Calculating T3";
-    //            showUI(formula_text, title_text, formula_illus[1]);
-
-    //            //TODO: Make UI look great for all scale
-    //            _player.transform.position = new Vector3(7.5f, 3.5f, 3);
-    //            _player.transform.rotation = new Quaternion(0, 1, 0, -1);
-    //        }
-
-    //        if (showTriangle_T2)
-    //        {
-
-    //            triangles = tri_4;
-
-    //            showT2(H);
-
-    //            formula_UI.transform.position = new Vector3(-5, 8.9f, 7.2f);
-    //            formula_UI.transform.rotation = new Quaternion(0, 0, 0, 0);
-
-    //            string T2_formula = "h<sup>2</sup> = H<sup>2</sup> - (width/2)height<sup>2</sup>";
-    //            string T2_title = "Calculating T2";
-    //            showUI(T2_formula, T2_title, formula_illus[2]);
-
-    //            _player.transform.position = new Vector3(-5, 2.8f, -8);
-    //            _player.transform.rotation = new Quaternion(0, 0, 0, 0);
-    //        }
-
-    //    }
-    //}
 
     public void showCalculation(int buttonType)
     {
 
-        //homeScreen.SetActive(false);
-        //formula_UI.SetActive(true);
-        //tutorialScreen.SetActive(false);
 
         targetScreen(formula_UI);
-
-        //_player.transform.position = new Vector3(0,0,-1);
-        //_player.transform.rotation = new Quaternion(0, 0, 0, 0);
 
         mesh.Clear();
 
@@ -471,42 +214,8 @@ public class ProceduralMesh : MonoBehaviour
         }
         else
         {
-            //homeScreen.SetActive(false);
             if (state == 0)
             {
-                //triangles = tri_1;
-
-                //Vector3[] R1_length_verts = new Vector3[] { vertices[4], vertices[1] };
-
-                //Vector3 label_tri1_h = new Vector3((x_0 - width) / 2 - 0.75f, (y_0 + height) / 2, z_0 + 1.0f / 3.0f * length);
-                //Vector3 label_tri1_w = new Vector3((vertices[1].x + vertices[11].x) / 2, y_0 - 0.5f, (vertices[1].z + vertices[11].z) / 2 + 0.5f);
-                //Vector3 label_R1 = new Vector3((vertices[1].x + vertices[4].x) / 2 + 2.25f, (vertices[4].y) / 2, vertices[4].z - 0.5f);
-
-                //string R1_text = "R1 = " + R1.ToString();
-
-                //makeLabel(labels[0], Label_inside_1_h, height.ToString());
-                //makeLabel(labels[1], Label_inside_1_w, width.ToString());
-                //makeLabel(labels[2], R1_length_verts, R1_text);
-
-                //labels[0].transform.position = label_tri1_h;
-                //labels[1].transform.position = label_tri1_w;
-                //labels[2].transform.position = label_R1;
-
-
-                //labels[0].transform.rotation = new Quaternion(0, 0, 0, 0);
-                //labels[1].transform.rotation = new Quaternion(0, 0, 0, 0);
-                //labels[2].transform.rotation = new Quaternion(0, 0, 0, 0);
-
-
-                //formula_UI.transform.position = new Vector3(-5f, 8.9f, 12.7f);
-                //formula_UI.transform.rotation = new Quaternion(0, 0, 0, 0);
-
-                //string R1_formula = "R1<sup>2</sup> = height<sup>2</sup> + (width/2)<sup>2</sup>";
-                //string R1_title = "Calculating R1";
-                //showUI(R1_formula, R1_title, formula_illus[0]);
-
-                //_player.transform.position = new Vector3(-5, 3.5f, -3);
-                //_player.transform.rotation = new Quaternion(0, 0, 0, 0);
                 clearlabel();
                 showR1(tri_1);
             }
@@ -525,7 +234,6 @@ public class ProceduralMesh : MonoBehaviour
                 string title_text = "Calculating T3";
                 showUI(formula_text, title_text, formula_illus[1]);
 
-                //TODO: Make UI look great for all scale
                 _player.transform.position = new Vector3(7.5f, 3.5f, 3);
                 _player.transform.rotation = new Quaternion(0, 1, 0, -1);
             }
@@ -534,7 +242,6 @@ public class ProceduralMesh : MonoBehaviour
             {
                 clearlabel();
 
-                // TODO: fixed T2 not showing h and H if R1 and T3 is not employed
                 triangles = tri_4;
 
                 showT2(H);
@@ -556,9 +263,6 @@ public class ProceduralMesh : MonoBehaviour
 
     public void showEntireRoof()
     {
-        //homeScreen.SetActive(true);
-        //formula_UI.SetActive(false);
-        //tutorialScreen.SetActive(false);
 
         targetScreen(homeScreen);
 
@@ -814,18 +518,106 @@ public class ProceduralMesh : MonoBehaviour
         {
             homeScreen.transform.position = away;
             tutorialScreen.transform.position = away;
+            Cust_UI.transform.position = away;
         }
 
         if (target == tutorialScreen)
         {
             formula_UI.transform.position = away;
             homeScreen.transform.position = away;
+            Cust_UI.transform.position = away;
+
         }
 
-        if(target == homeScreen)
+        if (target == homeScreen)
         {
             formula_UI.transform.position = away;
             tutorialScreen.transform.position = away;
+            Cust_UI.transform.position = away;
+
         }
+
+        if (target == Cust_UI)
+        {
+            homeScreen.transform.position = away;
+            formula_UI.transform.position = away;
+            tutorialScreen.transform.position = away;
+        }
+    }
+
+    public void showCustomisationUI()
+    {
+        targetScreen(Cust_UI);
+
+        _player.transform.position = Vector3.zero;
+        Cust_UI.transform.position = new Vector3(0, 0, 4);
+    }
+
+    /// 
+   
+    public void selectButton(int button)
+    {
+        cust_button_state = button;
+    }
+
+    /// <summary>
+    /// Allow user to specify the measurements of the roof 
+    /// </summary>
+    /// <param name="button">(int) 0 or 1, 0 is substration, 1 is addition </param>
+    /// <param name="measurement">(int) 0, 1 or 2, 0 is length, 1 is width, 2 is height </param>
+    public void customisation(int measurement)
+    {
+        //width = 1; 
+        //length = 1;
+        //height = 1;
+        Cust_UI.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = length.ToString();
+        Cust_UI.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = width.ToString();
+        Cust_UI.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = height.ToString();
+
+        if (width-1 > 0 && length-1 > 0 && height-1 > 0)
+        {
+            if (cust_button_state == 0)
+            {
+                if (measurement == 0)
+                {
+                    length -= 1f;
+                    Cust_UI.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = length.ToString();
+                }
+                if (measurement == 1)
+                {
+                    width -= 1f;
+                    Cust_UI.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = width.ToString();
+                }
+                if (measurement == 2)
+                {
+                    height -= 1f;
+                    Cust_UI.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = height.ToString();
+                }
+            }
+        }
+
+        if (cust_button_state == 1)
+        {
+            if (measurement == 0)
+            {
+                length += 1f;
+                Cust_UI.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = length.ToString();
+
+            }
+            if (measurement == 1)
+            {
+                width += 1f;
+                Cust_UI.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = width.ToString();
+
+            }
+            if (measurement == 2)
+            {
+                height += 1f;
+                Cust_UI.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = height.ToString();
+
+            }
+        }
+
+
     }
 }
